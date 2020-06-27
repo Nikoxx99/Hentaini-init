f254a16dbd8d0645c995a831bcebccd5da8d2d7d
Nicolas <nikoxx9900@gmail.com>
2020-06-26 19:27:12 -0500

    Spanish Traslations
    - Added Spanish traslations

--- frontend/lang/es.js
+++ frontend/lang/es.js
@@ -1,3 +1,20 @@
+// es-ES.js with Spanish Traslations
 export default {
-  linkExplore: 'Explorar'
+  menu: {
+    explore: 'EXPLORA',
+    airing: 'EMISIÓN',
+    suggestions: 'SUGERENCIAS'
+  },
+  mobile: {
+    home: 'Inicio',
+    airing: 'Emisión',
+    suggestions: 'Mi Lista'
+  },
+  episode: {
+    episode_number: 'Episodio',
+    download_text: 'Descargar Episodio',
+    add_favorite: 'Favorito',
+    show_episodes: 'Mostrar Episodios',
+    comments_section_header: 'Comentarios'
+  }
 }
